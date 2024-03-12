dd63bc9aacca16ceaa0a34c9ab9028e5f2532da1
armaanansari121 <armaanm2a@gmail.com>
2024-03-12 07:55:06 +0530

    Added a timer feature

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ import Question from "./components/Question";
 import NextButton from "./components/NextButton";
 import Progress from "./components/Progress";
 import FinishedScreen from "./components/FinishedScreen";
+import Timer from "./components/Timer";
+import Footer from "./components/Footer";
+
+const SECS_PER_QUESTION = 15;
 
 const initialState = {
   questions: [],
@@ -18,6 +22,7 @@ const initialState = {
   answer: null,
   points: 0,
   highscore: 0,
+  secondsRemaining: null,
 };
 
 function reducer(state, action) {
@@ -28,16 +33,20 @@ function reducer(state, action) {
         questions: action.payload,
         status: "ready",
       };
+
     case "dataFailed":
       return {
         ...state,
         status: "error",
       };
+
     case "start":
       return {
         ...state,
         status: "active",
+        secondsRemaining: state.questions.length * SECS_PER_QUESTION,
       };
+
     case "newAnswer":
       const question = state.questions.at(state.index);
 
@@ -49,25 +58,39 @@ function reducer(state, action) {
             ? state.points + question.points
             : state.points,
       };
+
     case "nextQuestion":
       return {
         ...state,
         index: state.index + 1,
         answer: null,
       };
+
     case "finished":
+      console.log(Math.max(state.highscore, state.points));
       return {
         ...state,
         status: "finished",
         highscore: Math.max(state.highscore, state.points),
       };
+
     case "restart":
       return {
-        ...state,
+        ...initialState,
+        questions: state.questions,
+        highscore: state.highscore,
         status: "ready",
-        index: 0,
-        answer: null,
-        points: 0,
+      };
+
+    case "tick":
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining <= 0 ? "finished" : state.status,
+        highscore:
+          state.secondsRemaining <= 0
+            ? Math.max(state.highscore, state.points)
+            : state.highscore,
       };
     default:
       throw new Error("Unknown action");
@@ -75,8 +98,10 @@ function reducer(state, action) {
 }
 
 function App() {
-  const [{ questions, status, index, answer, points, highscore }, dispatch] =
-    useReducer(reducer, initialState);
+  const [
+    { questions, status, index, answer, points, highscore, secondsRemaining },
+    dispatch,
+  ] = useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
   const maxPoints = questions.reduce(
@@ -121,13 +146,16 @@ function App() {
               dispatch={dispatch}
               answer={answer}
             />
-            {answer !== null && (
-              <NextButton
-                dispatch={dispatch}
-                index={index}
-                numQuestions={numQuestions}
-              />
-            )}
+            <Footer>
+              <Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
+              {answer !== null && (
+                <NextButton
+                  dispatch={dispatch}
+                  index={index}
+                  numQuestions={numQuestions}
+                />
+              )}
+            </Footer>
           </>
         )}
         {status === "finished" && (
